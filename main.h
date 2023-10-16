17c7301a2a9d08fb91c992ef5fb4c0581af2ca5d
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-16 22:19:00 +0100

    added files

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <limits.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _stringlength(const char *str);
 
 #endif/*MAIN_H*/
