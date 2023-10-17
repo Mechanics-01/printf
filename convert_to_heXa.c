db46b3bcc6876e7d4d79a0da284508ed4be8f4a3
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-18 00:38:55 +0100

    added file

---
+++ convert_to_heXa.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * convert_to_heXa - Converts an integer to hexadecimal and prints it.
+ * @value_bin: The integer to be converted and printed in hexadecimal.
+ *
+ * Return: Number of hexadecimal digits printed.
+ */
+
+int convert_to_heXa(int value_bin)
+{
+	int box[100];
+	int count_b = 0;
+	int divided;
+	char digits;
+	int i = 0;
+	int j = 0;
+
+	if (value_bin == 0)
+	{
+		_putchar('0');
+		count_b++;
+		return (count_b);
+	}
+
+	while (value_bin > 0)
+	{
+		divided = value_bin % 16;
+		if (divided < 10)
+			digits = divided + '0';
+		else
+			digits = (divided - 10) + 'A';
+
+		box[i] = digits;
+
+		value_bin = value_bin / 16;
+		i++;
+		count_b++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(box[j]);
+
+	return (count_b);
+}
