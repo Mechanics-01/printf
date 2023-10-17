ad6ec77b24a03bb541769f0de5536e0df8fe6f78
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-17 13:09:48 +0100

    -

--- _print001.c
+++ _print001.c
@@ -13,6 +13,8 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
+	if (*format == '\0')
+		return (-1);
 	va_start(args_list, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
