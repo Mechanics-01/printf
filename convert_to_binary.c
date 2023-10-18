6872dc9b04ba2222adc0848bc2e13325b6bb369a
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-18 19:24:59 +0100

    added

--- convert_to_binary.c
+++ convert_to_binary.c
@@ -4,38 +4,33 @@
  * convert_to_binary - Converts an integer to binary and prints it.
  * @value_bin: The integers to be converted and printed in binary.
  *
- * Return: Numbers of binary digits are printed.
+ * Return: Numbers of 32 binary digits are printed.
  */
 
 int convert_to_binary(long int value_bin)
 {
-	int box[200];
-	int count_b = 0;
-	int i = 0;
-	int j = 0;
+	char box[33];
+	int count_b = 31;
+	int i = 0, j;
+	int count = 0;
 
-	if (value_bin == 0)
-	{
-		_putchar('0');
-		count_b++;
-		return (count_b);
-	}
 	if (value_bin < 0)
 	{
-		_putchar('-');
-		count_b++;
-		value_bin = -value_bin;
+		box[0] = '1';
+		value_bin = -value_bin - 1;
 	}
-	while (value_bin > 0)
+	else
+		box[0] = '0';
+
+	for (i = 0; i < 31; i++)
 	{
-		box[i] = value_bin % 2;
-		value_bin = value_bin / 2;
-		i++;
-		count_b++;
+		box[count_b--] = (value_bin % 2) + '0';
+		value_bin = value_bin / 2; 
+		count++;
 	}
-
-	for (j = i - 1; j >= 0; j--)
-		_putchar(48 + box[j]);
-
-	return (count_b);
+	for (j = 0; j < 32; j++)
+		_putchar(box[j]);
+		
+	box[32] = '\0';
+	return (count);
 }
