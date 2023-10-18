382bac4f0051d690dae9b9c11ef00c94fb0878f6
Olusegun Francis <olusegunfrank.com>
2023-10-18 00:28:49 -0700

    gii

--- convert_to_octal.c
+++ convert_to_octal.c
@@ -2,7 +2,7 @@
 
 /**
  * convert_to_octal - Converts an integer to octal and prints in digit.
- * @value_o: The integer to be converted and printed in octal.
+ * @value_o: The integers to be converted and printed in octal.
  *
  * Return: Number of octal in digit is printed.
  */
