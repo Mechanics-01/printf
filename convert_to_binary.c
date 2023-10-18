5fe3d37e2f526dc9499aa8c711e6537093e6a8df
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-18 20:46:50 +0100

    solved for binary for 32 bits

--- convert_to_binary.c
+++ convert_to_binary.c
@@ -3,7 +3,6 @@
 /**
  * convert_to_binary - Converts an integer to binary and prints it.
  * @value_bin: The integers to be converted and printed in binary.
- *
  * Return: Numbers of 32 binary digits are printed.
  */
 
@@ -12,10 +11,10 @@ int convert_to_binary(long int value_bin)
 	char box[32];
 	int count_b = 0;
 	int i = 0, j;
-	
+
 	if (value_bin == 0)
 		_putchar('0');
-	
+
 	while (value_bin > 0)
 	{
 		box[i] = value_bin % 2;
@@ -25,7 +24,6 @@ int convert_to_binary(long int value_bin)
 	}
 	for (j = i - 1; j >= 0; j--)
 		_putchar(48 + box[j]);
-	
 
 	if (value_bin < 0)
 	{
@@ -36,9 +34,9 @@ int convert_to_binary(long int value_bin)
 			value_bin = value_bin / 2;
 			count_b++;
 		}
-	
+
 		for (j = i - 1; j >= 0; j--)
 			_putchar(48 + box[j]);
-	}	
+	}
 	return (count_b);
 }
