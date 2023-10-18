8082b9bf11ee4a26fbd0131e81ae356bdb6901d3
Olusegun Francis <olusegunfrank.com>
2023-10-18 00:18:42 -0700

    put

--- _putchar.c
+++ _putchar.c
@@ -5,7 +5,7 @@
  * @c: The character to print
  *
  * Return: On Success 1, otherwise 0.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error: -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
