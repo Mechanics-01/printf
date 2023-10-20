af97ad6d66947602322d85887093957378bf8d21
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-20 15:45:44 +0100

    added file

--- main.h
+++ main.h
@@ -6,7 +6,6 @@
 #include <limits.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdint.h>
 #define BUFF 1024
 
 
@@ -16,7 +15,7 @@ int _stringlength(const char *str);
 int convert_to_string(long int number);
 int convert_to_binary(long int value_bin);
 int convert_to_heXa(int value_bin);
-int convert_to_hexa(long int value_bin);
+int convert_to_hexa(int value_bin);
 int convert_to_octal(int value_o);
 int _putchar(char c);
 int format_specifier(va_list args_list, char format, int a, int b, int c);
