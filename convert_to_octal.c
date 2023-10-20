16d20fed2f4c1bcffbebee1522c67c086b764ee7
Olusegun Francis <olusegunfrank.com>
2023-10-19 20:51:24 -0700

    hid

--- convert_to_octal.c
+++ convert_to_octal.c
@@ -4,7 +4,7 @@
  * convert_to_octal - Converts an integer to octal and prints in digit.
  * @value_o: The integers to be converted and printed in octal.
  *
- * Return: Numbers of octals in digit is printed.
+ * Returns: Numbers of octals in digit is printed.
  */
 
 int convert_to_octal(int value_o)
