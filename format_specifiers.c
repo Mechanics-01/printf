9066c7a89209b406a34e7ddccadeff8dd0204108
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-21 10:41:53 +0100

    added files

--- format_specifiers.c
+++ format_specifiers.c
@@ -149,7 +149,7 @@ int format_specifier(va_list args_list, char format, int flag_h, int flag_p, int
 		else
 		{
 			write(1, "(nil)", 5);
-			count_n = count_n + 3;
+			count_n = count_n + 5;
 		}
 	}
 	else
